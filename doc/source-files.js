8d3b75cadf951e06154ca7d9cdf74cb2d3c58c38
Jonathan Woollett-Light <jonathanwoollettlight@gmai.com>
2020-08-09 03:31:49 +0100

    Deleted rust doc

--- doc/source-files.js
+++
@@ -1,3 +0,0 @@
-var N = null;var sourcesIndex = {};
-sourcesIndex["vulkano_blas"] = {"name":"","files":["level_1.rs","level_2.rs","level_3.rs","lib.rs"]};
-createSourceSidebar();
--- doc/vulkano_blas/saxpy/ty/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"struct":[["Buffer0",""],["Buffer1",""],["PushConstantData",""]]});
